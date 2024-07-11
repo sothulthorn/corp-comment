3ceb7d81f952fef6ddedee66ba011d9e7ff06404
sothulthorn <thornsothul@gmail.com>
2024-07-11 21:23:20 +0900

    Add input text counter

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+import { MAX_CHARACTERS } from '../lib/constants';
+
 const FeedbackForm = () => {
+  const [text, setText] = useState('');
+
+  const charCount = MAX_CHARACTERS - text.length;
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = event.target.value;
+    if (newText.length > MAX_CHARACTERS) {
+      return;
+    }
+    setText(event.target.value);
+  };
+
   return (
     <form className="form">
       <textarea
+        value={text}
         id="feedback-textarea"
         placeholder="placeholder"
         spellCheck={false}
+        onChange={handleChange}
       />
 
       <label htmlFor="feedback-textarea">
@@ -12,7 +29,7 @@ const FeedbackForm = () => {
       </label>
 
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
